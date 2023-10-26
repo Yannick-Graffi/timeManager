6135866efa8dab163e01f96dd7ae543ea2287c7a
Nicoforcio <nicolasbacon.nb@gmail.com>
2023-10-26 11:54:22 +0200

    use /api/clocks/:userId to ceate clocks with userID and status change

--- lib/timeManager_web/controllers/clock_controller.ex
+++ lib/timeManager_web/controllers/clock_controller.ex
@@ -4,16 +4,24 @@ defmodule TimeManagerWeb.ClockController do
   import Ecto.Query
 
   alias TimeManager.Accounts
-  alias TimeManager.Accounts.Clock
+  alias TimeManager.Accounts.{Clock, User}
   alias TimeManager.Repo
 
   action_fallback TimeManagerWeb.FallbackController
 
-  def create(conn, %{"clock" => clock_params}) do
-    with {:ok, %Clock{} = clock} <- Accounts.create_clock(clock_params) do
+  def create(conn, %{"userID" => userID}) do
+    %User{} = user = Accounts.get_user!(userID)
+
+    query = from c in Clock,
+                 where: c.users_id == ^userID,
+                 order_by: [desc: c.time],
+                 limit: 1
+    most_recent_clock = Repo.one(query)
+
+    with {:ok, %Clock{} = clock} <- Accounts.create_clock(%{status: !most_recent_clock.status, time: DateTime.truncate(DateTime.utc_now(), :second), users_id: userID}) do
       conn
       |> put_status(:created)
-      |> put_resp_header("location", ~p"/api/clocks/#{clock}")
+      |> put_resp_header("location", ~p"/api/clocks/#{userID}")
       |> render(:show, clock: clock)
     end
   end
