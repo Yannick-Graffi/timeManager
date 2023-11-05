896658f48d1dc987e56278ff2c1054191fef97ed
Nicoforcio <nicolasbacon.nb@gmail.com>
2023-11-05 22:21:06 +0100

    customize error message for invalid token

--- lib/timeManager/error_handler.ex
+++ lib/timeManager/error_handler.ex
@@ -1,8 +1,14 @@
 defmodule TimeManager.Guardian.ErrorHandler do
   import Plug.Conn
 
-  def auth_error(conn, {type, _reason}, _opts) do
-    body = Jason.encode!(%{error: to_string(type)})
+  def auth_error(conn, {:invalid_token, _reason}, _opts) do
+    body = Jason.encode!(%{error: "Invalid token"})
     send_resp(conn, 401, body)
   end
+  def auth_error(conn, {type, _reason}, _opts) do
+    message = to_string(type)
+    body = Jason.encode!(%{error: message})
+    send_resp(conn, 401, body)
+  end
+
 end
