876cc70489ff030d4465856e2d2db3fe14ec6e5c
LedouxK <ledoux.kevin@yahoo.com>
2023-11-09 15:51:28 +0100

    https management & certificate generation

--- lib/timeManager/access_token.ex
+++ lib/timeManager/access_token.ex
@@ -1,49 +1,52 @@
 defmodule TimeManager.Plugs.AccessToken do
   import Plug.Conn
 
+  # Initializes the plug with options, but no initialization is needed in this case
   def init(options), do: options
 
+  # Main function of the plug, checks and manages authentication using an access token
   def call(conn, _options) do
     conn
-    |> get_access_token_from_cookie()
-    |> verify_access_token()
+    |> get_access_token_from_cookie()  # Retrieve the access token from cookies
+    |> verify_access_token()           # Verify the validity of the access token
   end
 
+  # Private function to retrieve the access token from cookies
   defp get_access_token_from_cookie(conn) do
     cookies = conn.cookies()
     token = cookies["guardian_default_token"]
 
     if token do
-      assign(conn, :guardian_token, token)
+      assign(conn, :guardian_token, token)  # Assigns the access token to the connection
     else
-      conn
+      conn  # Returns the connection unchanged if no token is found
     end
   end
 
+  # Private function to verify the validity of the access token
   defp verify_access_token(conn) do
     case conn.assigns[:guardian_token] do
       nil ->
-        # Si le token n'est pas trouvé, renvoyer un statut d'erreur
+        # If token is not found, send an error status
         conn
         |> send_resp(401, "Authentication failed")
         |> halt()
 
       token ->
-        # Vérifier la validité du token
+        # Check the validity of the token
         case Guardian.decode_and_verify(TimeManager.Auth.Guardian, token) do
           {:ok, claims} ->
-            # Le token est valide, vous pouvez continuer avec le traitement de la requête
-            # et éventuellement assigner l'utilisateur à la connexion, si nécessaire.
+            # Token is valid, you can proceed with request processing
+            # and potentially assign claims to the connection, if needed.
             assign(conn, :claims, claims)
 
           {:error, reason} ->
-            # Le token n'est pas valide, logger la raison et renvoyer un statut d'erreur
-            dbg(reason)
+            # Token is not valid, log the reason and send an error status
+            dbg(reason)  # Logs the error reason (for debugging purposes)
             conn
             |> send_resp(401, "Authentication failed")
             |> halt()
         end
     end
   end
-
 end
