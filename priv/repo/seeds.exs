bb9e8e105aeb0484c8fcdb35d351d9e95e2ed7f6
Su <sulian.cornou@gmail.com>
2023-11-12 23:24:21 +0100

    Add more fixtures

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -19,12 +19,20 @@ import Ecto.Query
 
 ################## Insert USER ##################
 users = [
-  %{username: "john_doe1", email: "johna@example.com", password: "Azerty123456.", role: :employee},
-  %{username: "john_doe", email: "john@example.com", password: "Azerty123456.", role: :employee},
+  %{username: "john_doe", email: "johna@example.com", password: "Azerty123456.", role: :employee},
   %{username: "jane_smith", email: "jane@example.com", password: "Azerty123456.", role: :employee},
-  %{username: "bob_jones", email: "bob@example.com", password: "Azerty123456.", role: :manager},
-  %{username: "alice_brown", email: "alice@example.com", password: "Azerty123456.", role: :manager},
-  %{username: "jane_doe", email: "jane2@example.com", password: "Azerty123456.", role: :general_manager},
+  %{username: "jane_doe", email: "jane2@example.com", password: "Azerty123456.", role: :employee},
+  %{username: "michael_mayne", email: "michaelmayne@example.com", password: "Azerty123456.", role: :employee},
+  %{username: "emily_johnson", email: "emily.johnson@example.com", password: "Azerty123456.", role: :employee},
+  %{username: "michael_davis", email: "michael.davis@example.com", password: "Azerty123456.", role: :employee},
+  %{username: "jessica_taylor", email: "jessica.taylor@example.com", password: "Azerty123456.", role: :employee},
+  %{username: "christopher_brown", email: "christopher.brown@example.com", password: "Azerty123456.", role: :employee},
+  %{username: "amanda_wilson", email: "amanda.wilson@example.com", password: "Azerty123456.", role: :employee},
+  %{username: "daniel_moore", email: "daniel.moore@example.com", password: "Azerty123456.", role: :employee},
+  %{username: "sarah_anderson", email: "sarah.anderson@example.com", password: "Azerty123456.", role: :employee},
+  %{username: "megan_jackson", email: "megan.jackson@example.com", password: "Azerty123456.", role: :employee},
+  %{username: "kevin_white", email: "kevin.white@example.com", password: "Azerty123456.", role: :manager},
+  %{username: "alice_brown", email: "alice@example.com", password: "Azerty123456.", role: :general_manager},
   %{username: "admin2", email: "admin@example.com", password: "Azerty123456.", role: :admin},
 ]
 
@@ -41,11 +49,11 @@ end)
 
 ################## Insert TEAM ##################
 teams = [
-  %{name: "Alpha", manager_id: "5"},
-  %{name: "Bravo", manager_id: "5"},
-  %{name: "Charlie", manager_id: "5"},
-  %{name: "Delta", manager_id: "6"},
-  %{name: "Echo", manager_id: "6"},
+  %{name: "Alpha", manager_id: "14"},
+  %{name: "Bravo", manager_id: "14"},
+  %{name: "Charlie", manager_id: "14"},
+  %{name: "Delta", manager_id: "15"},
+  %{name: "Echo", manager_id: "15"},
 ]
 
 Enum.each(teams, fn team_attrs ->
@@ -82,29 +90,51 @@ end)
 ################## Insert WORKING TIME ##################
 now = DateTime.utc_now()
 
-nowAddOneDay = DateTime.add(now, 1, :day)
-nowAddTwoDay = DateTime.add(now, 2, :day)
+tomorrow = DateTime.add(now, 1, :day)
+afterTomorrow = DateTime.add(now, 2, :day)
+afterThreeDays = DateTime.add(now, 3, :day)
+afterFourDays = DateTime.add(now, 4, :day)
 
 workingTimes = [
-  %{start: now, end: DateTime.add(now, 5, :hour), user_id: "2"},
-  %{start: now, end: DateTime.add(now, 5, :hour), user_id: "3"},
-  %{start: now, end: DateTime.add(now, 5, :hour), user_id: "4"},
+  %{start: now, end: DateTime.add(now, 3, :hour), user_id: "2"},
+  %{start: now, end: DateTime.add(now, 4, :hour), user_id: "3"},
+  %{start: now, end: DateTime.add(now, 6, :hour), user_id: "4"},
+  %{start: now, end: DateTime.add(now, 2, :hour), user_id: "5"},
+  %{start: now, end: DateTime.add(now, 3, :hour), user_id: "6"},
+  %{start: now, end: DateTime.add(now, 4, :hour), user_id: "7"},
+  %{start: now, end: DateTime.add(now, 3, :hour), user_id: "8"},
 
-  %{start: DateTime.add(now, 6, :hour), end: DateTime.add(now, 10, :hour), user_id: "2"},
-  %{start: DateTime.add(now, 6, :hour), end: DateTime.add(now, 10, :hour), user_id: "3"},
-  %{start: DateTime.add(now, 6, :hour), end: DateTime.add(now, 10, :hour), user_id: "4"},
+  %{start: DateTime.add(now, 7, :hour), end: DateTime.add(now, 11, :hour), user_id: "3"},
+  %{start: DateTime.add(now, 8, :hour), end: DateTime.add(now, 9, :hour), user_id: "5"},
+  %{start: DateTime.add(now, 7, :hour), end: DateTime.add(now, 10, :hour), user_id: "7"},
+  %{start: DateTime.add(now, 7, :hour), end: DateTime.add(now, 10, :hour), user_id: "8"},
+  %{start: DateTime.add(now, 8, :hour), end: DateTime.add(now, 12, :hour), user_id: "9"},
 
-  %{start: nowAddOneDay, end: DateTime.add(nowAddOneDay, 5, :hour), user_id: "2"},
-  %{start: nowAddOneDay, end: DateTime.add(nowAddOneDay, 5, :hour), user_id: "3"},
-  %{start: nowAddOneDay, end: DateTime.add(nowAddOneDay, 5, :hour), user_id: "4"},
+  %{start: tomorrow, end: DateTime.add(tomorrow, 2, :hour), user_id: "2"},
+  %{start: tomorrow, end: DateTime.add(tomorrow, 3, :hour), user_id: "3"},
+  %{start: tomorrow, end: DateTime.add(tomorrow, 3, :hour), user_id: "4"},
+  %{start: tomorrow, end: DateTime.add(tomorrow, 4, :hour), user_id: "6"},
+  %{start: tomorrow, end: DateTime.add(tomorrow, 6, :hour), user_id: "8"},
+  %{start: tomorrow, end: DateTime.add(tomorrow, 4, :hour), user_id: "9"},
+  %{start: tomorrow, end: DateTime.add(tomorrow, 3, :hour), user_id: "10"},
+  %{start: tomorrow, end: DateTime.add(tomorrow, 2, :hour), user_id: "11"},
+  %{start: tomorrow, end: DateTime.add(tomorrow, 2, :hour), user_id: "12"},
 
-  %{start: nowAddTwoDay, end: DateTime.add(nowAddTwoDay, 3, :hour), user_id: "2"},
-  %{start: nowAddTwoDay, end: DateTime.add(nowAddTwoDay, 3, :hour), user_id: "3"},
-  %{start: nowAddTwoDay, end: DateTime.add(nowAddTwoDay, 3, :hour), user_id: "4"},
+  %{start: afterTomorrow, end: DateTime.add(afterTomorrow, 3, :hour), user_id: "2"},
+  %{start: afterTomorrow, end: DateTime.add(afterTomorrow, 2, :hour), user_id: "4"},
+  %{start: afterTomorrow, end: DateTime.add(afterTomorrow, 5, :hour), user_id: "6"},
+  %{start: afterTomorrow, end: DateTime.add(afterTomorrow, 5, :hour), user_id: "7"},
+  %{start: afterTomorrow, end: DateTime.add(afterTomorrow, 4, :hour), user_id: "9"},
+  %{start: afterTomorrow, end: DateTime.add(afterTomorrow, 3, :hour), user_id: "12"},
+  %{start: afterTomorrow, end: DateTime.add(afterTomorrow, 5, :hour), user_id: "13"},
+  %{start: afterTomorrow, end: DateTime.add(afterTomorrow, 3, :hour), user_id: "14"},
 
-  %{start: DateTime.add(nowAddTwoDay, 5, :hour), end: DateTime.add(nowAddTwoDay, 7, :hour), user_id: "2"},
-  %{start: DateTime.add(nowAddTwoDay, 5, :hour), end: DateTime.add(nowAddTwoDay, 7, :hour), user_id: "3"},
-  %{start: DateTime.add(nowAddTwoDay, 5, :hour), end: DateTime.add(nowAddTwoDay, 7, :hour), user_id: "4"},
+  %{start: DateTime.add(afterThreeDays, 6, :hour), end: DateTime.add(afterThreeDays, 8, :hour), user_id: "6"},
+  %{start: DateTime.add(afterThreeDays, 6, :hour), end: DateTime.add(afterThreeDays, 8, :hour), user_id: "7"},
+  %{start: DateTime.add(afterThreeDays, 8, :hour), end: DateTime.add(afterThreeDays, 9, :hour), user_id: "8"},
+  %{start: DateTime.add(afterThreeDays, 8, :hour), end: DateTime.add(afterThreeDays, 9, :hour), user_id: "9"},
+  %{start: DateTime.add(afterThreeDays, 7, :hour), end: DateTime.add(afterThreeDays, 9, :hour), user_id: "12"},
+  %{start: DateTime.add(afterThreeDays, 7, :hour), end: DateTime.add(afterThreeDays, 9, :hour), user_id: "13"},
 ]
 
 Enum.each(workingTimes, fn working_time_attrs ->
@@ -120,12 +150,7 @@ end)
 
 ################## Insert USER TEAM ##################
 
-# priv/repo/seeds.exs
-
-alias TimeManager.Repo
-alias TimeManager.Accounts.{User, Team}
-
-# ... le reste de votre code ...
+# Team 1
 
 # Fetch some users and teams by their id or other attributes
 team_1 = Repo.get!(Team, 1) # replace with actual team id
@@ -134,7 +159,7 @@ team_1 = Repo.get!(Team, 1) # replace with actual team id
 team_1 = Repo.preload(team_1, :users)
 
 # Assuming you have a list of user IDs that you want to add to the team
-user_ids = [2, 3, 4] # replace with actual user ids
+user_ids = [2, 3, 4, 5, 6, 7] # replace with actual user ids
 users = Repo.all(from u in User, where: u.id in ^user_ids)
 
 # Associate users with a team using preloaded users
@@ -148,3 +173,29 @@ case Repo.update(team_changeset) do
   {:error, changeset} ->
     IO.puts("Error adding users to the team: #{changeset}")
 end
+
+# Team 2
+
+# Fetch some users and teams by their id or other attributes
+team_2 = Repo.get!(Team, 2) # replace with actual team id
+
+# Preload the users association on the team
+team_2 = Repo.preload(team_2, :users)
+
+# Assuming you have a list of user IDs that you want to add to the team
+user_ids = [8, 9, 10, 11, 12, 13] # replace with actual user ids
+users = Repo.all(from u in User, where: u.id in ^user_ids)
+
+# Associate users with a team using preloaded users
+team_changeset = Ecto.Changeset.change(team_2)
+                 |> Ecto.Changeset.put_assoc(:users, users)
+
+# Save the changes
+case Repo.update(team_changeset) do
+  {:ok, _team} ->
+    IO.puts("Users have been added to the team")
+  {:error, changeset} ->
+    IO.puts("Error adding users to the team: #{changeset}")
+end
+
+
